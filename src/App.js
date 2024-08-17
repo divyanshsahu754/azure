087ddcbb54bfae667fb27d195d36e5220bffe9ad
Divyansh-SahuDeeptimaan <divyansh.sahu@deeptimaaninfosystem.com>
2024-08-17 11:07:18 +0530

    azure

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react"
+import axios from 'axios'
+
+
 
 function App() {
+  const [value, setValue] = useState("")
+  async function formSubmit(e){
+    e.preventDefault()
+    const {data} = await axios.get(
+      'https://reactfunction.azurewebsites.net/api/HttpTrigger1?code=Ay1fgDMvuAVzItwXw3YCrOeRKgRf70VKnD5WOV4TKnbqAzFuFwmsnQ%3D%3D', {
+        params: {
+          
+          name: value
+        }
+      }
+    );
+    alert(data)
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <form onSubmit={formSubmit}>
+        <input type="text"  value={value} onChange={(e) => setValue(e.target.value)} placeholder='Name' />
+        <button>Submit</button>
+        </form>
+        <p>{value}</p>
       </header>
     </div>
   );
